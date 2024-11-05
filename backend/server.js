13f24180c056df4e15b8b0b0c3060ee3c3205c9f
ebinberlin2 <ebinberlin@gmail.com>
2024-11-05 20:11:53 +0530

    vercel

--- backend/server.js
+++ backend/server.js
@@ -31,8 +31,16 @@ connectDB();
 
 
 
-app.use(cors()); 
+app.use(cors(
+  { 
+    origin: ["https://deploy-mern-1whq.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true
+  }
+)); 
 app.use(express.json());
+
+mongoose.connect('mongodb+srv://admindb:1234@cluster0.chq3co9.mongodb.net/admindbase?retryWrites=true&w=majority&appName=Cluster0');
 
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
